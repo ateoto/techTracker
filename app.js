c89ccf5a1bc0d04445bd353654ff54fbe30d2d56
Andrew Owen <andrew@moduscreate.com>
2015-02-20 13:54:30 -0500

    added images to the page as well as filters

--- app.js
+++ app.js
@@ -9,17 +9,33 @@
 			{
 				name: 'iPhone 6',
 				maker: 'Apple',
+				price: 600,
 				quantity: 3,
 				description: 'white, 16GB',
+				checkIn: Date.now(),
+				checkOut: "",
 				inStock: true,
+				images:[
+					{
+						full: 'images/iphone6_full.png'
+					},
+				],
 		
 			},
 			{
 				name: 'Galaxy S5',
 				maker: 'Samsung',
+				price: 540,
 				quantity: 1,
 				description: 'Silver, 32GB Micro SD',
+				checkIn: Date.now(),
+				checkOut: "",
 				inStock: true,
+				images: [
+					{
+						full: 'images/galaxys5_full.png'
+					},
+				],	
 			}
 		];
 
