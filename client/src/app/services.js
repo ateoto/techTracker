519332178ff85fa3de4fa1bde00016648d3d3748
Andrew Owen <andrew@moduscreate.com>
2015-07-06 14:33:01 -0400

    setting up localstorage and active user for login

--- client/src/app/services.js
+++ client/src/app/services.js
@@ -36,8 +36,33 @@
 
 	app.service('loginService', function($http) {
 
-		this.loginSuccess = function(user) {
-			return $http.post('http://localhost:3000/api/login');
+		this.login = function(email, password) {
+			return $http.post('http://localhost:3000/api/login', {"email" : email, "password" : password});
+		}
+
+		this.setActiveUser = function(user) {
+			localStorage.setItem('activeUser', JSON.stringify(user));
+		}
+
+		this.getActiveUser = function() {
+			var retrievedUser = localStorage.getItem('activeUser');
+			return JSON.parse(retrievedUser);
+		}
+	})
+
+
+	
+
+
+	app.service('activeRecordService', function() {
+
+		this.getActiveUser = function() {
+
+		}
+
+
+		this.setActiveUser = function() {
+
 		}
 	})
 })();
